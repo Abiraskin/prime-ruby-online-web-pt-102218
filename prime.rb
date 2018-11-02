2d411c3093c1a22198cbc4d707812e5b087d7ae9
Abi Raskin <abiraskin@gmail.com>
2018-11-02 01:18:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,8 @@ def prime?(number)
   if number <= 1
     return false
   else
-    (2..Math.sqrt(number)).none? { |i| number % i != 0 }
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
   end
 end
