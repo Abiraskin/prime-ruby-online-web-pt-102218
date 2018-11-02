7bad7fbb6478d06a119246466e3de55f5a77c5ce
Abi Raskin <abiraskin@gmail.com>
2018-11-02 01:19:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   if number <= 1
     return false
   else
-    (2..num-1).to_a.all? do |possible_factor|
+    (2..number-1).to_a.all? do |possible_factor|
       number % possible_factor != 0
     end
   end
