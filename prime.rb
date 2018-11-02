ce2e9ed7fb50e0d9d12ed6b033c43ad03625559b
Abi Raskin <abiraskin@gmail.com>
2018-11-02 00:40:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,6 @@
 def prime?(number)
   return if number <= 1
-  (2..Math.sqrt(number)).none? { |i| (number % i).zero? }
+    (2..Math.sqrt(number)).none? { |i| (number % i).zero? }
+  else
+    false
 end
